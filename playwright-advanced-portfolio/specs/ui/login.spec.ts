c7919cd013c14030a6cdd2f5b0a8f41ca4d05500
SiddhardhaTankala <sid.tankala@gmail.com>
2026-07-21 21:34:41 +0530

    login for success

--- playwright-advanced-portfolio/specs/ui/login.spec.ts
+++ playwright-advanced-portfolio/specs/ui/login.spec.ts
@@ -2,44 +2,50 @@ import {test, expect} from '@playwright/test';
 import {LoginPage} from '../../pages/LoginPage';
 import {DataReader} from '../../utils/DataReader';
 
-test.describe('Authentication - UI Validation', () => {     //Group related tests together for better organization and reporting
+interface LoginTestData {
+    username: string;
+    password: string;
+    expected_result: string;
 
-    test('Should display an error message for invalid login credentials', async ({page}) => {       //Test case to validate error message for invalid login credentials
+}
 
-        //1. Instantiate our Page object Model for the Login Page, passing in the Playwright page object to the constructor. This allows the LoginPage class to interact with the browser page.
-        const loginPage = new LoginPage(page);  
+test.describe("UI Authentication - Data Driven", () => {
+
+    const testCases = DataReader.getCsvData<LoginTestData>('users.csv');
+    
+
+
+    testCases.forEach((record, index) => {
+        const email = record.username?.trim() || ' ';
+        const password = record.password?.trim() || ' ';
+        const expectedResult = record.expected_result.trim() || 'failure';
         
-        // 2. Perform the action workflow using our encapsulated method
-        await loginPage.login('invalid@example.com', 'wrongpassword');
+        
 
-        // 3. Extract the error message string from the UI
-        const errorText = await loginPage.getErrorMessage();
+        test(`Login test #${index+1}: ${email} (${expectedResult})`, async({page}) => {
+            const loginPage = new LoginPage(page);
+            const path: string = process.env.BASE_URL || '';
 
-        // 4. Assert that the validation message contains the correct industry text
-        expect(errorText).toContain("Invalid email or password");
-    });
+            await loginPage.navigateTo(path);
+            await loginPage.login(email, password);
 
-    //test('Authentication - Data Driven UI Validation', () => {
-        //Fetch our parsed data array dynamically using the utility we built
-        const testData = DataReader.getCsvData<{username: string; password: string; expectedError: string}> ('users.csv');
+            if (expectedResult === 'success') {
+                await expect(page).toHaveURL('https://practicesoftwaretesting.com/admin/dashboard');
+                await expect(page.getByText("Sales over the years")).toBeVisible();
+                await page.locator('[data-test="nav-home"]').click();
+                await expect(page.getByRole('heading', { name: 'Filters' })).toBeVisible();
+            
+            } else {
+                const errorMessage = await loginPage.getErrorMessage();
+                expect(errorMessage).toBeTruthy();
 
-        // Loop through every row of data to dynamically generate tests
-        testData.forEach((row) => {
-            test(`Should reject login for user with data driven validation: ${row.username}`, async({page}) =>{
+            }
 
-                const loginPage = new LoginPage(page);
-
-                //Execute the action using data straight from the current loop row
-                loginPage.login(row.username, row.password);
-
-                //Extract and validate the UI error message response
-                const errorText = await loginPage.getErrorMessage();
-                expect(errorText).toContain(row.expectedError);
-            });
 
         });
+    });
 
 
-    //});
+
 
 });
